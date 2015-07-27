e1c96460ce7a3c42245bddba530cf451baa75ed2
SKrusteva <sterjanakrasteva@gmail.com>
2015-07-27 18:35:12 +0200

    Add homeworks

---
+++ ExtDelegatesLambdaLinq/02.IEnumerableExtension.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtDelegatesLambdaLinq
+{
+    /* 02. Implement a set of extension methods for IEnumerable<T> that implement the following group functions:
+     * sum, product, min, max, average. */
+
+    public static class IEnumerableExtension
+    {
+        public static T Sum<T>(this IEnumerable<T> collection)
+        {
+            T sum = (dynamic)0;
+            foreach (dynamic item in collection)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static T Product<T>(this IEnumerable<T> collection)
+        {
+            T product = (dynamic)1;
+            foreach (dynamic item in collection)
+            {
+                product *= item;
+            }
+
+            return product;
+        }
+
+        public static T Min<T>(this IEnumerable<T> collection) where T : struct
+        {
+            return collection.Min(); //stackoverflow?
+        }
+
+        public static T Max<T>(this IEnumerable<T> collection)
+        {
+            return collection.Max(); //stackoverflow?
+        }
+
+        public static T Average<T>(this IEnumerable<T> collection)
+        {
+            T average = (dynamic)collection.Sum() / collection.Count();
+            return average;
+        }
+    }
+}
---
+++ ExtDelegatesLambdaLinq/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ExtDelegatesLambdaLinq
+{ //[3],[4],[5]
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        private DateTime dateOfBirth;
+
+        public Student(string first, string last, DateTime birth)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.DateOfBirth = birth;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return this.dateOfBirth; }
+            set { this.dateOfBirth = value; }
+        }
+
+        public int Age()
+        {
+            int age = 0;
+            if (DateTime.Now.Month - this.DateOfBirth.Month < 0)
+            {
+                age = DateTime.Now.Year - this.DateOfBirth.Year - 1;
+            }
+            else if (DateTime.Now.Month - this.DateOfBirth.Month == 0)
+            {
+                if (DateTime.Now.Day - this.DateOfBirth.Day < 0)
+                {
+                    age = DateTime.Now.Year - this.DateOfBirth.Year - 1;
+                }
+                else
+                {
+                    age = DateTime.Now.Year - this.DateOfBirth.Year;
+                }
+            }
+            else if (DateTime.Now.Month - this.DateOfBirth.Month > 0)
+            {
+                age = DateTime.Now.Year - this.DateOfBirth.Year;
+            }
+
+            return age;
+        }
+
+        public override string ToString()
+        {
+            int years = this.Age();
+            
+            return this.FirstName + " " + this.LastName + ", " + years;
+        }
+    }
+}
